e27a73bdcf9e2f2a803cf81081202312149e5980
Myoungmin <miung222@gmail.com>
2022-06-25 00:54:22 +0900

    React - 15.2. State 사용
    App이 내부적으로 사용할 상태는 state라는 형태를 통해 조작한다.
    그리고 이렇게 만들어진 state 값을 Subject라는 Component의 props 값으로 넘겨줬다.
    즉, 상위 Component인 App의 상태를 하위 Component인 Subject로 전달하고 싶을 때는
    상위 Component의 state 값을 하위 Component의 props로 전달한다.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ import Content from './components/Content';
 import './App.css';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'world wide web!'}
+    }
+  }
+
   render(){
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
