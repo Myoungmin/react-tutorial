9defec61919b52509430a1c54c5703554575249c
Myoungmin <miung222@gmail.com>
2022-06-25 00:42:18 +0900

    React - 14. Component 파일로 분리하기
    각각의 Component 별로 별도의 파일로 분리한다.
    코드가 간결해지고 필요한 Component를 빨리 찾을 수 있고 App.js가 아닌 다른 React 파일에서도 파일로 분리한 Component들을 사용하여 빠르게 애플리케이션을 만들 수 있다.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
 //아래 Component import 해야 에러 발생하지 않음
 import React, { Component } from 'react';
+import Subject from './components/Subject';
+import TOC from './components/TOC';
+import Content from './components/Content';
 import './App.css';
-
-class Subject extends Component{
-  render(){
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component{
-  render(){
-    return (
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component{
-  render(){
-    return (
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
-    );
-  }
-}
 
 class App extends Component{
   render(){
