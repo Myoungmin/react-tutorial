f9e9d5ab24ab64f625902b70b0b5d3165ff656d4
Myoungmin <miung222@gmail.com>
2022-06-25 14:22:36 +0900

    React - 17.1. 컴포넌트 이벤트 만들기
    이벤트 생산자가 되기위한 실습.
    Subject라는 Component에 onChangePage라는 이벤트를 우리가 만들었다.
    그리고 그 이벤트에 함수를 설치한다.
    그 이벤트가 발생되었을 때 props로 전달된 onChangePage 연결된 이벤트핸들러 함수를 호출하면 된다.

--- src/App.js
+++ src/App.js
@@ -1,52 +1,45 @@
 //아래 Component import 해야 에러 발생하지 않음
 import React, { Component } from 'react';
-//import Subject from './components/Subject';
+import Subject from './components/Subject';
 import TOC from './components/TOC';
 import Content from './components/Content';
 import './App.css';
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      mode:'welcome',
-      subject:{title:'WEB', sub:'world wide web!'},
-      welcome:{title:'Welcome', desc:'Hello, React!!'},
-      contents:[
-        {id:1, title:'HTML', desc:'HTML is for information'},
-        {id:2, title:'CSS', desc:'CSS is for design'},
-        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+      mode: 'welcome',
+      subject: { title: 'WEB', sub: 'world wide web!' },
+      welcome: { title: 'Welcome', desc: 'Hello, React!!' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is for information' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' },
       ]
     }
   }
 
-  render(){
+  render() {
     var _title, _desc = null;
-    if(this.state.mode === 'welcome'){
+    if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     }
-    else if(this.state.mode === 'read'){
+    else if (this.state.mode === 'read') {
       _title = this.state.contents[0].title;
       _desc = this.state.contents[0].desc;
     }
     return (
       <div className="App">
-        {/* <Subject 
-          title={this.state.subject.title} 
-          sub={this.state.subject.sub}>
-        </Subject> */}
-        <header>
-          <h1><a href="/" onClick={function(e){
-            console.log(e);
-            e.preventDefault();
-            this.setState({
-              mode:'welcome'
-            });
-            //debugger;
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-          {this.state.subject.sub}
-        </header>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function () {
+            this.setState({ mode:'welcome' });
+          }.bind(this)}
+        >
+        </Subject>
         <TOC data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
